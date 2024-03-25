6e542bfebb0d46e3559e8ccf1c681b887f8ccfda
vipin-jangra <vipinsandwal10244@gmail.com>
2024-03-25 14:31:44 +0530

    Bus ticket

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import DefaultLayout from "./components/DefaultLayout";
-import "./App.css";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Navigate,
-} from "react-router-dom";
-import Reservation from "./pages/reservation";
-import Dashboard from "./pages/dashboard";
-import "./stylesheets/tables.css";
+import DefaultLayout from './components/DefaultLayout';
+import './App.css';
+import {BrowserRouter as Router, Routes, Route, Navigate} from 'react-router-dom';
+import Reservation from './pages/reservation';
+import Dashboard from './pages/dashboard';
+import "./stylesheets/tables.css"
+
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Routes>
-          <Route index element={<Navigate to="/reservation" />} />
-          <Route path="" element={<DefaultLayout />}>
-            <Route path="reservation" element={<Reservation />} />
-            <Route path="dashboard" element={<Dashboard />} />
+          <Route path='/' element={<Navigate to='/reservation' />} />
+          <Route path='/*' element={<DefaultLayout />} >
+            <Route path='reservation' element={<Reservation />} />
+            <Route path='dashboard' element={<Dashboard />} />
           </Route>
+          {/* <Route path='/' element={<Navigate to={'/reservation'} />} />
+          <Route path='/reservation' element={<DefaultLayout><Reservation /></DefaultLayout>} />
+          <Route path='/dashboard' element={<DefaultLayout><Dashboard /></DefaultLayout>} /> */}
         </Routes>
       </Router>
     </div>
